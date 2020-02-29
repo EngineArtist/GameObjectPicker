e04e7cd49baf73c87b56184f2902ce5685bb61f0
Miika Vihersaari <miika.vihersaari@gmail.com>
2020-02-29 20:48:06 +0200

    Made utility class static

--- Editor/GameObjectPropertyDrawer.cs
+++ Editor/GameObjectPropertyDrawer.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 namespace EngineArtist {
 
 
-public class PropertyDrawerUtils {
+public static class GameObjectPropertyDrawerUtils {
     public static Dictionary<string, Texture2D> icons = new Dictionary<string, Texture2D>();
     public static bool objectPickerMode = false;
     public static int pickedDrawerID = 0;
@@ -19,7 +19,7 @@ public class PropertyDrawerUtils {
     }
 
     public static void UpdateCallback(SceneView view) {
-        if (PropertyDrawerUtils.objectPickerMode) {
+        if (GameObjectPropertyDrawerUtils.objectPickerMode) {
             var cur = Event.current;
             switch (cur.type) {
                 case EventType.Layout: {
@@ -27,8 +27,8 @@ public class PropertyDrawerUtils {
                     break;
                 }
                 case EventType.MouseUp: {
-                    PropertyDrawerUtils.pickedObject = HandleUtility.PickGameObject(Event.current.mousePosition, true);
-                    PropertyDrawerUtils.objectPickerMode = false;
+                    GameObjectPropertyDrawerUtils.pickedObject = HandleUtility.PickGameObject(Event.current.mousePosition, true);
+                    GameObjectPropertyDrawerUtils.objectPickerMode = false;
                     UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
                     break;
                 }
@@ -53,35 +53,35 @@ public class PropertyDrawerUtils {
 [CustomPropertyDrawer(typeof(GameObject))]
 public class GameObjectPropertyDrawer: PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        ++PropertyDrawerUtils.currentPropertyDrawerID;
-        if (!PropertyDrawerUtils.callbackRegistered) {
-            PropertyDrawerUtils.callbackRegistered = true;
-            EditorApplication.update -= PropertyDrawerUtils.ResetPropertyDrawerID;
-            EditorApplication.update += PropertyDrawerUtils.ResetPropertyDrawerID;
-            SceneView.duringSceneGui -= PropertyDrawerUtils.UpdateCallback;
-            SceneView.duringSceneGui += PropertyDrawerUtils.UpdateCallback;
+        ++GameObjectPropertyDrawerUtils.currentPropertyDrawerID;
+        if (!GameObjectPropertyDrawerUtils.callbackRegistered) {
+            GameObjectPropertyDrawerUtils.callbackRegistered = true;
+            EditorApplication.update -= GameObjectPropertyDrawerUtils.ResetPropertyDrawerID;
+            EditorApplication.update += GameObjectPropertyDrawerUtils.ResetPropertyDrawerID;
+            SceneView.duringSceneGui -= GameObjectPropertyDrawerUtils.UpdateCallback;
+            SceneView.duringSceneGui += GameObjectPropertyDrawerUtils.UpdateCallback;
         }
-        if (!PropertyDrawerUtils.objectPickerMode && PropertyDrawerUtils.pickedDrawerID == PropertyDrawerUtils.currentPropertyDrawerID) {
-            property.objectReferenceValue = PropertyDrawerUtils.pickedObject;
-            PropertyDrawerUtils.pickedObject = null;
-            PropertyDrawerUtils.pickedDrawerID = 0;
+        if (!GameObjectPropertyDrawerUtils.objectPickerMode && GameObjectPropertyDrawerUtils.pickedDrawerID == GameObjectPropertyDrawerUtils.currentPropertyDrawerID) {
+            property.objectReferenceValue = GameObjectPropertyDrawerUtils.pickedObject;
+            GameObjectPropertyDrawerUtils.pickedObject = null;
+            GameObjectPropertyDrawerUtils.pickedDrawerID = 0;
         }
         EditorGUI.BeginProperty(position, label, property);
         property.objectReferenceValue = EditorGUI.ObjectField(new Rect(position.x, position.y, position.width - position.height, position.height), label, property.objectReferenceValue, typeof(GameObject), true);
         var pickerRect = new Rect(position.x + position.width - position.height, position.y, position.height, position.height);
         if (GUI.Button(pickerRect, "")) {
-            PropertyDrawerUtils.objectPickerMode = !PropertyDrawerUtils.objectPickerMode;
-            if (PropertyDrawerUtils.objectPickerMode) {
-                PropertyDrawerUtils.pickedDrawerID = PropertyDrawerUtils.currentPropertyDrawerID;
+            GameObjectPropertyDrawerUtils.objectPickerMode = !GameObjectPropertyDrawerUtils.objectPickerMode;
+            if (GameObjectPropertyDrawerUtils.objectPickerMode) {
+                GameObjectPropertyDrawerUtils.pickedDrawerID = GameObjectPropertyDrawerUtils.currentPropertyDrawerID;
             }
             else {
-                PropertyDrawerUtils.pickedDrawerID = 0;
+                GameObjectPropertyDrawerUtils.pickedDrawerID = 0;
             }
         }
         var pickerOffsetX = pickerRect.width / 8f;
         var pickerOffsetY = pickerRect.height / 8f;
         var pickerIconRect = new Rect(pickerRect.x + pickerOffsetX, pickerRect.y + pickerOffsetY, pickerRect.width - pickerOffsetX*2f, pickerRect.height - pickerOffsetY*2f);
-        GUI.DrawTexture(pickerIconRect, PropertyDrawerUtils.GetIcon("object_picker"), ScaleMode.ScaleAndCrop, true);
+        GUI.DrawTexture(pickerIconRect, GameObjectPropertyDrawerUtils.GetIcon("object_picker"), ScaleMode.ScaleAndCrop, true);
         EditorGUI.EndProperty();
     }
 }
