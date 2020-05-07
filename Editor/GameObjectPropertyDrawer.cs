e8c1a788e051bfcf612beb40a3f9d85e2b883f65
Miika Vihersaari <miika.vihersaari@gmail.com>
2020-05-07 21:38:24 +0300

    Bugfix: Picking an object for a field would set it for a different field

--- Editor/GameObjectPropertyDrawer.cs
+++ Editor/GameObjectPropertyDrawer.cs
@@ -9,14 +9,10 @@ namespace EngineArtist {
 public static class GameObjectPropertyDrawerUtils {
     public static Dictionary<string, Texture2D> icons = new Dictionary<string, Texture2D>();
     public static bool objectPickerMode = false;
-    public static int pickedDrawerID = 0;
+    public static string pickedPropertyPath = "";
+    public static GameObjectPropertyDrawer pickedDrawer = null;
     public static GameObject pickedObject = null;
     public static bool callbackRegistered = false;
-    public static int currentPropertyDrawerID = 0;
-
-    public static void ResetPropertyDrawerID() {
-        currentPropertyDrawerID = 0;
-    }
 
     public static void UpdateCallback(SceneView view) {
         if (GameObjectPropertyDrawerUtils.objectPickerMode) {
@@ -53,18 +49,18 @@ public static class GameObjectPropertyDrawerUtils {
 [CustomPropertyDrawer(typeof(GameObject))]
 public class GameObjectPropertyDrawer: PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        ++GameObjectPropertyDrawerUtils.currentPropertyDrawerID;
         if (!GameObjectPropertyDrawerUtils.callbackRegistered) {
             GameObjectPropertyDrawerUtils.callbackRegistered = true;
-            EditorApplication.update -= GameObjectPropertyDrawerUtils.ResetPropertyDrawerID;
-            EditorApplication.update += GameObjectPropertyDrawerUtils.ResetPropertyDrawerID;
             SceneView.duringSceneGui -= GameObjectPropertyDrawerUtils.UpdateCallback;
             SceneView.duringSceneGui += GameObjectPropertyDrawerUtils.UpdateCallback;
         }
-        if (!GameObjectPropertyDrawerUtils.objectPickerMode && GameObjectPropertyDrawerUtils.pickedDrawerID == GameObjectPropertyDrawerUtils.currentPropertyDrawerID) {
+        if (!GameObjectPropertyDrawerUtils.objectPickerMode &&
+            GameObjectPropertyDrawerUtils.pickedPropertyPath == property.propertyPath &&
+            GameObjectPropertyDrawerUtils.pickedDrawer == this) {
             property.objectReferenceValue = GameObjectPropertyDrawerUtils.pickedObject;
             GameObjectPropertyDrawerUtils.pickedObject = null;
-            GameObjectPropertyDrawerUtils.pickedDrawerID = 0;
+            GameObjectPropertyDrawerUtils.pickedPropertyPath = "";
+            GameObjectPropertyDrawerUtils.pickedDrawer = null;
         }
         EditorGUI.BeginProperty(position, label, property);
         property.objectReferenceValue = EditorGUI.ObjectField(new Rect(position.x, position.y, position.width - position.height, position.height), label, property.objectReferenceValue, typeof(GameObject), true);
@@ -72,10 +68,12 @@ public class GameObjectPropertyDrawer: PropertyDrawer {
         if (GUI.Button(pickerRect, "")) {
             GameObjectPropertyDrawerUtils.objectPickerMode = !GameObjectPropertyDrawerUtils.objectPickerMode;
             if (GameObjectPropertyDrawerUtils.objectPickerMode) {
-                GameObjectPropertyDrawerUtils.pickedDrawerID = GameObjectPropertyDrawerUtils.currentPropertyDrawerID;
+                GameObjectPropertyDrawerUtils.pickedPropertyPath = property.propertyPath;
+                GameObjectPropertyDrawerUtils.pickedDrawer = this;
             }
             else {
-                GameObjectPropertyDrawerUtils.pickedDrawerID = 0;
+                GameObjectPropertyDrawerUtils.pickedPropertyPath = "";
+                GameObjectPropertyDrawerUtils.pickedDrawer = null;
             }
         }
         var pickerOffsetX = pickerRect.width / 8f;
